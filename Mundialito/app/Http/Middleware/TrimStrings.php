0b0446721bd6ce1b90c0c55d09c59df787d0cc01
Luis Rendon Zuniga <Luis Rendon Zuniga>
2018-06-27 19:24:14 -0500

    empezando

---
+++ Mundialito/app/Http/Middleware/TrimStrings.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mundialito\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\TrimStrings as Middleware;
+
+class TrimStrings extends Middleware
+{
+    /**
+     * The names of the attributes that should not be trimmed.
+     *
+     * @var array
+     */
+    protected $except = [
+        'password',
+        'password_confirmation',
+    ];
+}
