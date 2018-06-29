dd8f9857f41a64458a3e2ad0470f0e8c77b2e62b
Luis Rendon Zuniga <Luis Rendon Zuniga>
2018-06-29 12:40:07 -0500

    Magonesa

--- Mundialito/resources/views/inicio.blade.php
+++ Mundialito/resources/views/inicio.blade.php
@@ -65,6 +65,7 @@
         </style>
     </head>
     <body>
+        <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -91,5 +92,6 @@
                 </div>
             </div>
         </div>
+    </body>
     </body>
 </html>
