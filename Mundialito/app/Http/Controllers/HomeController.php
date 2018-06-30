2a9ef418bec8f1ccc989db555aea87ca76b392e8
Luis Rendon Zuniga <Luis Rendon Zuniga>
2018-06-29 20:20:58 -0500

    Nickname en la pagina de inicio

--- Mundialito/app/Http/Controllers/HomeController.php
+++ Mundialito/app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace Mundialito\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $nickname=DB::table('users')->where('id','=',Auth::id())->value('nickname');
+        $data=['nickname'=>$nickname];
+        return view('home',$data);
     }
 }
