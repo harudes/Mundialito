49e4fd293830c56bc5e575d0f9f2088027304b53
VergilGX <fabiosotomejia@gmail.com>
2018-06-30 12:29:20 -0500

    Figuritas Readys

--- Mundialito/resources/views/myalbum.blade.php
+++ Mundialito/resources/views/myalbum.blade.php
@@ -9,17 +9,12 @@
               <div class="col-sm-6 col-md-4">
                 <div class="thumbnail">
                   <img src="../img/jugadores/{{$figurita->imagen}}.png"  alt="..." height="200" width="150">
-                  <div class="caption">
-                    <p>...</p>
-                    <p>
-                    </p>
-                  </div>
                 </div>
               </div>
             </div>
          @endforeach
          @for($i=1;$i< 33;$i++)
-            <a href="{{route('myalbum',['page' => $i])}}" class="btn btn-outline mw-md rounded btn-primary">{{$i}}</a>
+            <a href="{{route('myalbum',['page' => $i])}}" class="btn btn-outline mw-md rounded btn-primary" style="background: url('../img/Banderas/Bandera{{$i}}.png') ; background-size: 38px; width: 40px; height: 40px" ></a>
          @endfor
     </div>
 </div>
--- Mundialito/resources/views/myfigurines.blade.php
+++ Mundialito/resources/views/myfigurines.blade.php
@@ -8,12 +8,6 @@
               <div class="col-sm-6 col-md-4">
                 <div class="thumbnail">
                   <img src="img/jugadores/{{$figurita->imagen}}.png"  alt="..." height="200" width="150">
-                  <div class="caption">
-                    <h3>{{$figurita->nombre}}</h3>
-                    <p>...</p>
-                    <p>
-                    </p>
-                  </div>
                 </div>
               </div>
             </div>
