f78d0a1b0a3a9885cd00550c741506070fc4ad1b
Luis Rendon Zuniga <Luis Rendon Zuniga>
2018-06-30 12:28:07 -0500

    Abrimos paquetes

--- Mundialito/app/Http/Controllers/OpenPackageController.php
+++ Mundialito/app/Http/Controllers/OpenPackageController.php
@@ -12,14 +12,50 @@ class OpenPackageController extends Controller
         $paquetes = DB::table('users')
         			->join('paquetes','paquetes.user_id','=','users.id')
         			->join('tipo_paquete','paquetes.rareza','=','tipo_paquete.id')
-        			->where('users.id','=',Auth::id())->get()
-        			/*->value('figuritas.id')*/;
+        			->where('users.id','=',Auth::id())->get();
         $data = ['paquetes'=>$paquetes];
         return view('openpackage',$data);
     }
 
     public function abrir($rareza){
-        switch ($rareza) {
+    	$cantidad=DB::table('figuritas')->count();
+    	//echo $cantidad;
+    	for($i=1;$i<=5;$i++){
+    		$figurita=rand(1, $cantidad);
+    		$aux=DB::table('users')
+    			->join('figuritas_x_usuario','figuritas_x_usuario.user_id','=','users.id')
+    			->join('figuritas','figuritas.id','=','figuritas_x_usuario.figurita_id')
+    			->where('figuritas.id','=',$figurita)
+	            ->where(function ($query) {
+	                $query->where('users.id','=',Auth::id());
+	            })->count();
+    		if($aux==0){
+	            DB::table('figuritas_x_usuario')->insert([
+	            'user_id' => Auth::id(),
+	            'figurita_id' => $figurita,
+	            'cantidad'=>1,
+	            'pegada'=>0,
+	        	]);
+        	}
+	        	else{
+	        		$tiene=DB::table('users')
+			    			->join('figuritas_x_usuario','figuritas_x_usuario.user_id','=','users.id')
+			    			->join('figuritas','figuritas.id','=','figuritas_x_usuario.figurita_id')
+			    			->where('figuritas.id','=',$figurita)
+				            ->where(function ($query) {
+				                $query->where('users.id','=',Auth::id());
+				            })->value('figuritas_x_usuario.cantidad');
+	        		DB::table('users')
+	    			->join('figuritas_x_usuario','figuritas_x_usuario.user_id','=','users.id')
+	    			->join('figuritas','figuritas.id','=','figuritas_x_usuario.figurita_id')
+	    			->where('figuritas.id','=',$figurita)
+		            ->where(function ($query) {
+		                $query->where('users.id','=',Auth::id());
+		            })->update(['figuritas_x_usuario.cantidad' => $tiene +1]);
+	        	}		
+    		
+    	}
+        /*switch ($rareza) {
 		    case 0:
 		        echo "i es igual a 0";
 		        break;
@@ -32,7 +68,7 @@ class OpenPackageController extends Controller
 		    case 3:
 		        echo "i es igual a 2";
 		        break;
-		}
+		}*/
     }
 
     public function openPackage($rareza){
