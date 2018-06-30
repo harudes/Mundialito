379f45b57103ffab18e9dcd0b22c2dad0d6adb71
Luis Rendon Zuniga <Luis Rendon Zuniga>
2018-06-29 23:50:33 -0500

    Ahora si muestra la imagen

--- Mundialito/resources/views/myalbum.blade.php
+++ Mundialito/resources/views/myalbum.blade.php
@@ -8,7 +8,7 @@
             <div class="row">
               <div class="col-sm-6 col-md-4">
                 <div class="thumbnail">
-                  <img src="img/{{$figurita->imagen}}.png"  alt="..." height="300" width="270">
+                  <img src="../img/{{$figurita->imagen}}.png"  alt="..." height="300" width="270">
                   <div class="caption">
                     <p>...</p>
                     <p>
